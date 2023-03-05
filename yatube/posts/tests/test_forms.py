1aa304ac81f1e8121ec7dfa8fe53f745b3025a0b
Екатерина Балабаева <balabaeva.e.yu@yandex.ru>
2023-03-05 19:15:42 +0300

    Sprint 6 - Project - the last version

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -16,19 +16,6 @@ class PostCreateFormTests(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        test_gif = (
-            b'\x47\x49\x46\x38\x39\x61\x02\x00'
-            b'\x01\x00\x80\x00\x00\x00\x00\x00'
-            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
-            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
-            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
-            b'\x0A\x00\x3B'
-        )
-        image_test = SimpleUploadedFile(
-            name='test_image.gif',
-            content=test_gif,
-            content_type='image_test/gif',
-        )
         cls.user = User.objects.create_user(
             username='test_form_user',
         )
@@ -43,7 +30,6 @@ class PostCreateFormTests(TestCase):
         cls.post = Post.objects.create(
             author=cls.user,
             text='Text_form',
-            image=image_test,
         )
         cls.comment = Comment.objects.create(
             post=cls.post,
@@ -97,7 +83,7 @@ class PostCreateFormTests(TestCase):
         post = Post.objects.first()
         self.assertEqual(post.text, form_data['text'])
         self.assertEqual(post.group.pk, form_data['group'])
-        self.assertEqual(f'posts/{post_image}', f'posts/{post_image}')
+        self.assertEqual(post.image, f'posts/{post_image}')
 
     def test_post_edit(self):
         """Проверка формы редактирования поста."""
